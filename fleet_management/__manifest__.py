aa8da4e242c58b5d6cd51ededf715b0027569bb8
Pratima Gupta <pgu@odoo.com>
2025-08-06 16:58:43 +0530

    [ADD] exe 1: added module

---
+++ fleet_management/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Fleet Management',
+    'version': '1.0',
+    'category': 'Operations',
+    'summary': 'Track company vehicles, maintenance, and scheduling.',
+    'description': """
+Custom Fleet Management
+========================
+Manage vehicles, track maintenance schedules, and automate service reminders.
+""",
+    'author': 'Odoo PS',
+    'website': 'https://yourcompany.com',
+    'category': 'Operations/Fleet',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    'demo': [],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
