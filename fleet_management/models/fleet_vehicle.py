da97e786c89a24b178f561999cebc742b0bd0983
Pratima Gupta <pgu@odoo.com>
2025-08-06 18:57:15 +0530

    [ADD] exe 3: added fields to fleet_vehicle model
    [ADD] exe 3: added fields to fleet_vehicle model

--- fleet_management/models/fleet_vehicle.py
+++ fleet_management/models/fleet_vehicle.py
@@ -3,3 +3,17 @@ from odoo import models, fields
 class FleetVehicle(models.Model):
     _name = 'fleet.vehicle'
     _description = 'Fleet Vehicles'
+
+    name = fields.Char(required=True)
+    brand = fields.Char()
+    model = fields.Char()
+    license_plate = fields.Char()
+    purchase_date = fields.Date()
+    last_service_date = fields.Date(string="Last Service Date")
+    service_interval_days = fields.Integer(default=180)
+    odometer_km = fields.Float()
+    status = fields.Selection([
+        ('active', 'Active'),
+        ('sold', 'Sold')
+        ])
+    maintenance_cost = fields.Float()
