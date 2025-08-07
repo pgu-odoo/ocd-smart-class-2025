af26d9dc82cb949d0d1ca390e30277ef9b1895db
Pratima Gupta <pgu@odoo.com>
2025-08-07 12:00:06 +0530

    [ADD] exe 7: added attributes for status and purchase_date field

--- fleet_management/models/fleet_vehicle.py
+++ fleet_management/models/fleet_vehicle.py
@@ -8,12 +8,12 @@ class FleetVehicle(models.Model):
     brand = fields.Char()
     model = fields.Char()
     license_plate = fields.Char()
-    purchase_date = fields.Date()
+    purchase_date = fields.Date(copy=False)
     last_service_date = fields.Date(string="Last Service Date")
     service_interval_days = fields.Integer(default=180)
     odometer_km = fields.Float()
     status = fields.Selection([
         ('active', 'Active'),
         ('sold', 'Sold')
-        ])
+        ], default='active', readonly=True)
     maintenance_cost = fields.Float()
